36857e4705cd96de24f4cc5e9d949b103d225078
Glenn <gleenn@gmail.com>
2011-09-05 10:54:44 -0700

    problem 2

---
+++ prob2.scala
@@ -0,0 +1,19 @@
+#!/bin/sh
+exec scala $0 $@
+!#
+
+var sum = 0
+var prev = 0
+var cur = 1
+var temp = 0
+
+while(cur < 4000000) {
+	temp = cur
+	cur = prev + cur
+	prev = temp
+
+	if(cur % 2 == 0)
+		sum += cur
+}
+
+println(sum)
