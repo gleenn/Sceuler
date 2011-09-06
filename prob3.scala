b1081cd52b22e3eca558c75a63469f93573926cd
Glenn <gleenn@gmail.com>
2011-09-06 13:38:47 -0700

    continued wippy wip on problem 3

--- prob3.scala
+++ prob3.scala
@@ -2,11 +2,27 @@
 exec scala $0 $@
 !#
 
+import scala.collection.mutable.ArrayBuffer
+
 //def go(i : Int, f : Int => Unit) = { for( j <-0.until(i)) { f(j) } }
 
-def findPrimesUnder(num : Int) = {
-	var checkLimit = math.sqrt(num)
-	
+def findPrimesUpto(num : Int) = {
+	val checkLimit = math.sqrt(num).ceil.toInt + 1
+	val sieve = new Array[Boolean](checkLimit)
+	for(i : Int <- 2.until(checkLimit)) {
+		var currentMultiple = i + i
+		while(currentMultiple < checkLimit){ 
+			sieve(currentMultiple) = true
+			currentMultiple += i
+		}
+	}
+	sieve
 }
 
-println(findPrimesUnder(4))
+val expected20 = Array(false, false, false, false, true)
+assert(findPrimesUpto(20) == expected20)
+
+val expected100 = Array(false, false, false, false, true, false, true, false, true, true, true)
+assert(findPrimesUpto(100) == expected100)
+println(expected100)
+println(findPrimesUpto(100))
