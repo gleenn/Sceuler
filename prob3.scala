29ff2c7ece983fc067379aa3846a77ec1134fb11
Glenn <gleenn@gmail.com>
2011-09-11 19:19:27 -0700

    continued continued wippy wip on problem 3

--- prob3.scala
+++ prob3.scala
@@ -19,22 +19,44 @@ def buildSieve(max : Int) = {
 }
 
 def getPrimeFactors(max : Int) = {
-	buildSieve(max).zipWithIndex.filter{case(isFactorable,num) => !isFactorable}.map{case(isPrime,num)=>num}
+	buildSieve(max).zipWithIndex.filter{case(isFactorable,num) => !isFactorable}.map{case(isPrime,num)=>num+1}
 }
 
-def largestPrimeFactor(num : Int) = {
+def largestPrimeFactor(num : Int) : Int = {
 	val largestPossiblePrimeFactor = math.sqrt(num).ceil.toInt + 1
-	val primeFactorsToCheck = getPrimeFactors(largestPossiblePrimeFactor)
+	val primeFactorsToCheck = getPrimeFactors(largestPossiblePrimeFactor).reverse
+	for(possiblePrimeFactor : Int <- primeFactorsToCheck) {
+		if(num % possiblePrimeFactor == 0) return possiblePrimeFactor
+	}
+	1
 }
 
 //println(getPrimeFactors(10).toList)
 
+def test_largestPrimeFactor = {
+	val expectedFactors = List(1,2,3,2,5,3,7,4,3)
+	(1 to 10).map{
+	assert(largestPrimeFactor(1) == 1)
+	assert(largestPrimeFactor(2) == 2)
+	assert(largestPrimeFactor(3) == 3)
+	assert(largestPrimeFactor(4) == 2)
+	assert(largestPrimeFactor(5) == 5)
+	assert(largestPrimeFactor(6) == 3)
+	assert(largestPrimeFactor(7) == 7)
+	assert(largestPrimeFactor(8) == 4)
+	assert(largestPrimeFactor(9) == 3)
+	assert(largestPrimeFactor(10) == 5)
+	assert(largestPrimeFactor(100) == 5)
+}
+test_largestPrimeFactor
+
 def test_getPrimeFactors {
 	val expected10 = List(1, 2, 3, 5, 7)
 	val actual10 = getPrimeFactors(10).toList
+	println(actual10.zip(expected10))
 	assert(actual10 == expected10)
 }
-test_getPrimeFactors
+//test_getPrimeFactors
 
 def test_buildSieve {
 	val expected4 = List(false, false, false, true)
