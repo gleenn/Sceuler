61a0d2ebe7ab7a081ab0099dedcb2fea5fa9f658
Glenn <gleenn@gmail.com>
2011-09-18 03:01:01 -0700

    working problem 4

--- src/main/scala/prob4.scala
+++ src/main/scala/prob4.scala
@@ -1,14 +1,47 @@
 object Problem4 extends App {
-	def isPalindrome(a : Int, b : Int) : Boolean {
+  def isPalindrome(candidatePalindrome: Int): Boolean = {
+    var isPalindrome = true
+    val string = candidatePalindrome.toString
+    val length = string.length
+    for (i <- 0 until (length / 2)) {
+      if (string(i) != string(length - i - 1)) return false
+    }
+    isPalindrome
+  }
 
-	}
+  def maxPalindrome() = {
+    var maxPalindrome = 0
+    var candidatePalindrome = 0
+    for (i <- 100 to 999) {
+      for (j <- 100 to 999) {
+        candidatePalindrome = i * j
+        if (candidatePalindrome > maxPalindrome && isPalindrome(candidatePalindrome))
+          maxPalindrome = candidatePalindrome
+      }
+    }
+    maxPalindrome
+  }
 
-	def maxPalindrome
-	var maxPalindrome = 0
+//  test
+  println(maxPalindrome())
 
-	for (i <- 100 to 999) {
-		for (j <- 100 to 999) {
-			println (i * j).toString
-		}
-	}
+  def expect(palindrome: Int, expected: Boolean) {
+    val actual = isPalindrome(palindrome)
+    if (expected == actual) {
+      println(palindrome + " passed")
+    } else {
+      println(palindrome + " failed")
+    }
+  }
+
+  def test() {
+    expect(0, true)
+    expect(1, true)
+    expect(44, true)
+    expect(404, true)
+    expect(4004, true)
+
+    expect(17, false)
+    expect(117, false)
+  }
 }
