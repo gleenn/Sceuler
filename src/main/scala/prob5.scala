420acfa215f7984dd58abd449309c3c7aca55ad2
Glenn <gleenn@gmail.com>
2011-09-20 15:09:38 -0700

    fast, working problem 5

--- src/main/scala/prob5.scala
+++ src/main/scala/prob5.scala
@@ -1,30 +1,34 @@
 object prob5 extends App {
-
-  def smallestNumDivisibleBy(range : Range) : Int = {
-    def divides(num : Int, denom : Int) = {
-      println(num + " " + denom)
-      num % denom == 0
-    }
-    for(i <- 1 to range.fold(*)) {
-      if(range.forall(divides(i,_))) return i
-    }
-    -1
+  def gcd(a : Int, b : Int) : Int = {
+      if(b == 0) return a
+      gcd(b, a % b)
   }
 
-  println(smallestNumDivisibleBy(1 to 20))
+  def lcm(a : Int, b : Int) : Int = {
+    (a / gcd(a, b)) * b
+  }
+
+  def leastCommonMultiple(nums : Range) : Int = {
+    nums.foldLeft(1)((previousLCM, num) => lcm(previousLCM, num))
+  }
+
+  println(leastCommonMultiple(1 to 20))
 //  test
 
-  def expectedSmallest(expected : Int, divisors : Range) {
-    if(expected == smallestNumDivisibleBy(divisors)) {
-      println("passed smallestNumDivisibleBy(" + divisors + ")")
+  def expectedLCM(expected : Int, nums : Range) {
+    val actual: Int = leastCommonMultiple(nums)
+    if(expected == actual) {
+      println("passed leastCommonMultiple(" + nums + ")")
     } else {
-      println("failed smallestNumDivisibleBy(" + divisors + ")")
+      println("failed leastCommonMultiple(" + nums + ") != " + actual)
     }
   }
 
   def test() {
-    expectedSmallest(2, 1 to 2)
-    expectedSmallest(6, 1 to 3)
-    expectedSmallest(60, 1 to 5)
+    expectedLCM(2, 1 to 2)
+    expectedLCM(6, 1 to 3)
+    expectedLCM(60, 1 to 5)
+    expectedLCM(60, 1 to 6)
+    expectedLCM(420, 1 to 7)
   }
 }
